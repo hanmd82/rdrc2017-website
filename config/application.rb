f9994a560871235c6daa04697e7889f1980708ac
Mingding <hanmd82@gmail.com>
2017-01-01 17:27:16 +0800

    Remove secrets file and export as ENV var

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Rdrc2016
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
